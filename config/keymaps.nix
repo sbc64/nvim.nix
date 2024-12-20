caf165745092011441ca62ac544e48b492c34dcf
Sebastian Bolanos <git@sebas.tech>
2024-12-20 11:36:31 +0100

    nixpkgs-fmt

--- config/keymaps.nix
+++ config/keymaps.nix
@@ -1,4 +1,4 @@
-{lib, ...}: {
+{ lib, ... }: {
   plugins.lsp.keymaps = {
     silent = true;
     diagnostic = {
@@ -60,31 +60,31 @@
   };
   keymaps =
     lib.attrsets.mapAttrsToList
-    (
-      name: value: {
-        mode = "n";
-        key = name;
-        action.__raw = ''require("smart-splits").${value}'';
+      (
+        name: value: {
+          mode = "n";
+          key = name;
+          action.__raw = ''require("smart-splits").${value}'';
+        }
+      )
+      {
+        # for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
+        "<A-h>" = "resize_left";
+        "<A-j>" = "resize_down";
+        "<A-k>" = "resize_up";
+        "<A-l>" = "resize_right";
+        # Moving
+        "<C-h>" = "move_cursor_left";
+        "<C-j>" = "move_cursor_down";
+        "<C-k>" = "move_cursor_up";
+        "<C-l>" = "move_cursor_right";
+        "<C-\\>" = "move_cursor_previous";
+        #  swapping buffers between windows
+        "<leader><leader>h" = "swap_buf_left";
+        "<leader><leader>j" = "swap_buf_down";
+        "<leader><leader>k" = "swap_buf_up";
+        "<leader><leader>l" = "swap_buf_right";
       }
-    )
-    {
-      # for example `10<A-h>` will `resize_left` by `(10 * config.default_amount)`
-      "<A-h>" = "resize_left";
-      "<A-j>" = "resize_down";
-      "<A-k>" = "resize_up";
-      "<A-l>" = "resize_right";
-      # Moving
-      "<C-h>" = "move_cursor_left";
-      "<C-j>" = "move_cursor_down";
-      "<C-k>" = "move_cursor_up";
-      "<C-l>" = "move_cursor_right";
-      "<C-\\>" = "move_cursor_previous";
-      #  swapping buffers between windows
-      "<leader><leader>h" = "swap_buf_left";
-      "<leader><leader>j" = "swap_buf_down";
-      "<leader><leader>k" = "swap_buf_up";
-      "<leader><leader>l" = "swap_buf_right";
-    }
     ++ [
       {
         mode = "n";
